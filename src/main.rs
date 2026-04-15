481fdaba167b24989496cdd631fed99b40197ec5
brian <lbc.brian.chilin@gmail.com>
2026-04-14 22:00:11 -0700

    fixed centering math

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use ab_glyph::{FontRef, PxScale};
 use imageproc::image::{Rgba, RgbaImage};
 use imageproc::drawing::{draw_text_mut, text_size};
 use rand::Rng;
+use std::cmp;
 
 fn main() {
 	let render_me = "Hello world!\nfoobar";
-	let padding: i32 = 32; // pixels
+	let padding: i32 = 128; // pixels
     let height = 360.0;
     let scale = PxScale {
         x: height,
@@ -25,7 +26,8 @@ fn main() {
 
 	let mut lines: Vec<&str> = render_me.split('\n').collect();
 	let mut font_indices: Vec<Vec<u8>> = Vec::new(); // which font for each character
-	let mut widths: Vec<u32> = Vec::new(); 
+	let mut largest_width = 0;
+	let mut widths: Vec<u32> = Vec::new();
 	for line in &lines {
 		let mut line_indices: Vec<u8> = Vec::new();
 		let mut line_width = 0;
@@ -36,14 +38,12 @@ fn main() {
 		}
 		font_indices.push(line_indices);
 		widths.push(line_width);
+		largest_width = cmp::max(largest_width, line_width);
 	}
 	println!("{:?}\n{:?}\n{:?}", &lines, &font_indices, &widths);
 
-	let h = ((height+1.0) as i32 * lines.len() as i32) + (padding * 2);	
-	let canvas_width: u32 = match widths.iter().max() {
-		Some(w) => *w + (padding as u32 * 2),
-		None => panic!("no width"),
-	};
+	let h = ((height+1.0) as i32 * lines.len() as i32) + (padding * 2);
+	let canvas_width: u32 = largest_width + (padding as u32 * 2);
 	let mut image = RgbaImage::new(
 		canvas_width, 
 		u32::try_from(h).unwrap()
@@ -53,7 +53,7 @@ fn main() {
 	let (mut x, mut y) = (padding, padding);
 	for i in 0..lines.len() {
 		let line = lines[i];
-		x = padding + ((canvas_width - widths[i])/2) as i32;
+		x = padding + ((largest_width - widths[i])/2) as i32;
 		for j in 0..line.len() {
 			let char_as_string = line.chars().nth(j).unwrap().to_string();
 			let font = &fonts[font_indices[i as usize][j as usize] as usize].1;
